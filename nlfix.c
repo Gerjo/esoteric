0213622a2090f671dd7c2e6340e4dc2ec1f22674
Gerard <gerjoo@gmail.com>
2016-10-20 19:29:17 +0100

    refactor & read from file(s) directly.

--- nlfix.c
+++ nlfix.c
@@ -10,32 +10,71 @@
 /// example: echo -e "hello\rfoo" | nlfix
 /// example: cat myfile.txt | nlfix | tee out.txt
 
+typedef struct {
+	int win;
+	int unix;
+	int mac;
+} Stats;
+
+void analyse(FILE* file, int doOut, Stats* stats);
+void print_stats(Stats* stats);
+
 int main(int argc, char** argv) {
 
 	char buffer[128];
-	
-	int nWin = 0;
-	int nUnix = 0;
-	int nMac = 0;
 
-	int doOut = 1;
 	int doStats = 0;
 	
-	if(argc > 1 && strcmp(argv[1], "-s") == 0) {
-		doOut = 0;
-		doStats = 1;
+	for(int i = 1; i < argc; ++i) {
+		if(strcmp(argv[i], "-s") == 0) {
+			doStats = i;			
+			break;
+		}
 	}
+	
+	Stats stats = {0, 0, 0};
+	
+	if(argc > 2 || (!doStats && argc > 1)) {
+		
+		for(int i = 1; i < argc; ++i) {
+			if(i != doStats) {
+				FILE* file = fopen(argv[i], "r");
+				
+				if(file) {
+					analyse(file, !doStats, &stats);
+					
+					fclose(file);
+				} else {
+					fprintf(stderr, "Error: Could not open '%s' for reading.\n", argv[i]);
+				}
+			}
+		}
+		
+		// files.
+	} else {
+		analyse(stdin, !doStats, &stats);
+	}
+	
+	if(doStats) {
+		print_stats(&stats);
+	}
+	
+	return 0;
+}
+
+void analyse(FILE* file, int doOut, Stats* stats) {
+	char buffer[128];
 	
 	int retrieved = 0;
 	char last = 0;
 	do {
-		int retrieved = fread(buffer, 1, sizeof buffer, stdin);
+		int retrieved = fread(buffer, 1, sizeof buffer, file);
 	
 		for(int i = 0; i < retrieved; ++i) {
 			char current = buffer[i];
 						
 			if(current == '\r' && last == '\n') {
-				++nWin;
+				++stats->win;
 				
 				// Keep as-is. (don't mess up git)
 				if(doOut)
@@ -44,12 +83,12 @@ int main(int argc, char** argv) {
 			} else if(current == '\r') {
 				// withhold tentative Mac newline
 			} else if(last == '\r') {
-				++nMac;
+				++stats->mac;
 				
 				if(doOut)
 					fputc('\n', stdout);
 			} else if(current == '\n') {
-				++nUnix;
+				++stats->unix;
 				
 				if(doOut)
 					fputc(current, stdout);
@@ -60,26 +99,21 @@ int main(int argc, char** argv) {
 			
 			last = current;
 		}
-	} while( ! feof(stdin));
+	} while( ! feof(file));
 	
 	if(last == '\r') {
-		++nMac;
+		++stats->mac;
 	
 		if(doOut)
 			fputc('\n', stdout);
 	}
-	
-	
-	// \r 0x0d ^M 
-	// \n 0x0a ^J
-	
-	if(doStats) {
-		printf(" os   | escape | hex       | count \n");
-		printf("-------------------------------\n");
-		printf(" win  | \\n\\r   | 0x0a 0x0d | %d\n", nWin);
-		printf(" unix | \\n     | 0x0a (^J) | %d\n", nUnix);
-		printf(" mac  | \\r     | 0x0d (^M) | %d\n", nMac);
-	}
-	
-	return 0;
 }
+
+void print_stats(Stats* stats) {
+	printf(" os   | escape | hex       | count \n");
+	printf("-------------------------------\n");
+	printf(" win  | \\n\\r   | 0x0a 0x0d | %d\n", stats->win);
+	printf(" unix | \\n     | 0x0a (^J) | %d\n", stats->unix);
+	printf(" mac  | \\r     | 0x0d (^M) | %d\n", stats->mac);
+}
+
