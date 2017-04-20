61b44e82b47729bf795c242bb3a1747512eeb5f6
Gerard <gerjoo@gmail.com>
2017-04-20 18:26:56 +0100

    Run .cs files.

--- runit.py
+++ runit.py
@@ -3,6 +3,32 @@
 import sys, os, subprocess
 import os.path
 
+def run_cs(filename):
+    compilers = ["/Users/gerjo/extern/Applications/Unity/Unity.app/Contents/MonoBleedingEdge/bin/mcs"]
+    runtimes = ["/Users/gerjo/extern/Applications/Unity/Unity.app/Contents/MonoBleedingEdge/bin/mono"]
+    
+    compiler = runtime = None
+    
+    for bin in compilers:
+        if os.path.isfile(bin):
+            compiler = bin
+            
+    for bin in runtimes:
+        if os.path.isfile(bin):
+            runtime = bin
+    
+    if compiler == None or runtime == None:
+        print("Cannot run .cs files. Either runtime or compiler isn't found.")
+    
+    tmp = "out.exe";
+    
+    didCompile = run("{} -out:{} {}".format(compiler, tmp, filename))
+        
+    # Test for success exit code
+    if didCompile == 0:
+        run("{} {}".format(runtime, tmp))
+        run("rm -f {}".format(tmp))
+
 recipes = {}
 
 recipes[".cpp"] = "c++ -Wall -Wextra -std=c++14 {} -o out && ./out && rm ./out"
@@ -10,6 +36,7 @@ recipes[".c"]   = "gcc -Wall -Wextra -std=c11 {} -o out && ./out && rm ./out"
 recipes[".js"]  = "node {}"
 recipes[".py"]  = "python {}"
 recipes[".php"] = "php {}"
+recipes[".cs"]  = run_cs
 recipes[".m"]   = "clang -framework Foundation {} -o out && ./out && rm ./out"
 recipes[".mm"]  = "clang++ -std=c++14 -ObjC++ -framework Foundation {} -o out && ./out && rm ./out"
 
@@ -18,21 +45,26 @@ def error(code, str):
     sys.exit(code) 
 
 def run(cmd):
-    os.system(cmd)
+    return os.system(cmd)
 
 if len(sys.argv) == 2:
     filename = sys.argv[-1]
     pwd = os.getcwd()
     
-    #if os.access(filename, os.X_OK):
-    #    run(filename)
-    #else:
-    extension = os.path.splitext(filename)[1].lower();
-    
-    if extension in recipes:
-        run(recipes[extension].format(filename))
+    if os.access(filename, os.X_OK):
+        run("./" + filename)
     else:
-        error(2, "Cannot execute '{}' files. No known recipe.".format(extension))
+        extension = os.path.splitext(filename)[1].lower();
+    
+        if extension in recipes:
+            recipe = recipes[extension]
+            
+            if callable(recipes[extension]):
+                recipes[extension](filename)
+            else:
+                run(recipe.format(filename))
+        else:
+            error(2, "Cannot execute '{}' files. No known recipe.".format(extension))
     
 else:
     error(1, "Unknown number of arguments specified: {}".format(len(sys.argv)))
