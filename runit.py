0482e5d426730fbd84e4b2ee833a0c992ed3afb6
gerard <gerjoo@gmail.com>
2020-02-07 12:38:38 +0000

    updated help

--- runit.py
+++ runit.py
@@ -159,7 +159,7 @@ def main(args):
 parser = argparse.ArgumentParser(description="Execute any sort of file.", epilog="This ought to make it easier to quickly test something, right?")
 parser.add_argument("--recipe", help="The recipe to use in case file extension is ambiguous", dest="recipe", nargs="?", default=None)
 parser.add_argument("filename", help="The to be executed file")
-parser.add_argument("--entr", help="Monitor for file changes", dest="entr", action="store_const", default=False, const=True)
+parser.add_argument("--entr", help="Monitor for file changes (requires macOS or Linux)", dest="entr", action="store_const", default=False, const=True)
 parser.add_argument("--maxdepth", help="Recursion depth of find, in case entr is used", dest="maxdepth", action="store", default=2)
 parser.add_argument("--nobench", help="Remote benchmark and ruler", dest="bench", action="store_const", default=True, const=True)
 parser.add_argument("vars", nargs="*", help="optional arguments passed onto executed program")
