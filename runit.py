da1af9894d71aadba00f7c02cb18dbcd9b2c2a36
Gerard <gerjoo+git@gmail.com>
2020-06-04 22:47:51 +0100

    More explicit error handling based on how the recipe was deduced. Allow entr to monitor multiple executables. Automatically deduce ambiguous recipes. More structure by using json objects to store recipe meta data. Store full stop on extensions again (recipe ident and file extension are decoupled now).

--- runit.py
+++ runit.py
@@ -5,16 +5,103 @@ import os.path, fnmatch
 import argparse
 import time
 
+def get_recipes():
+    """Return all known recipes of this program"""
+    return [
+        {
+            "recipe": "cpp",
+            "ext": [".cpp", ".h"],
+            "run": "c++ -Wall -Wextra -std=c++14 {source} -o out && (./out {args}; rm ./out)"
+        },
+        {
+            "recipe": "c",
+            "ext": [".c", ".h"],
+            "run": "gcc -Wall -Wextra -std=c11 {source} -o out && (./out {args}; rm ./out)"
+        },
+        {
+            "recipe": "javascript",
+            "ext": [".js"],
+            "run": "node {source} {args}"
+        },
+        {
+            "recipe": "osascript",
+            "ext": [".scpt"],
+            "run": "osascript {source} {args}"
+        },
+        {
+            "recipe": "php",
+            "ext": [".php"],
+            "run": "php {source} {args}"
+        },
+        {
+            "recipe": "cs",
+            "ext": [".cs"],
+            "run": run_cs
+        },
+        {
+            "recipe": "sh",
+            "ext": [".sh"],
+            "run": "sh {source} {args}"
+        },
+        {
+            "recipe": "bash",
+            "ext": [".bash"],
+            "run": "bash {source} {args}"
+        },
+        {
+            "recipe": "zsh",
+            "ext": [".zsh"],
+            "run": "zsh {source} {args}"
+        },
+        {
+            "recipe": "objc++",
+            "ext": [".mm"],
+            "run": "clang++ -std=c++14 -ObjC++ -framework Foundation {source} -o out && (./out {args}; rm ./out)"
+        },
+        {
+            "recipe": "r",
+            "ext": [".r"],
+            "run": "/Library/Frameworks/R.framework/Resources/Rscript {source} {args}" 
+        },
+        {
+            "recipe": "npm",
+            "ext": [".js", ".html", ".css"],
+            "run": "npm start --prefix {source}"
+        },
+        {
+            "recipe": "python",
+            "ext": [".py", ".py3"],
+            "run": "python3 {source} {args}"
+        },
+        {
+            "recipe": "matlab",
+            "ext": [".m"],
+            "run": "matlab -nodisplay -nosplash -nodesktop -noFigureWindows -r \"try, run('{source}'), catch e, fprintf('%s\\n', e.message), end;exit(0);\""
+        },
+    	{
+            "recipe": "objc",
+            "ext": [".m"],
+            "run": "clang -framework Foundation {source} -o out && (./out {args}; rm ./out)"
+        },
+        {
+            "recipe": "java",
+            "ext": [".java"],
+            "run": run_java
+        },
+    ]
+
 def get_sub_args(args):
     return " ".join(args.args)
 
-def run_java(filename, extension, args):
-
-    executable = filename[0:-len(extension)-1]
+def run_java(filename, recipe, args):
+    """Execute a java source code file"""
+    
+    # Java uses the file's name to determine the entry point.
+    executable = os.path.splitext(filename)[0]
     
     cmd = "javac {} && java {} {} && rm {}.class".format(filename, executable, get_sub_args(args), executable)
     
-    run(cmd, extension, args)
+    run(cmd, recipe, args)
     
 def find_cs_binaries():
     roots = [
@@ -47,7 +134,7 @@ def find_cs_binaries():
                     
     return (compiler, runtime)
     
-def run_cs(filename, extension, args):
+def run_cs(filename, recipe, args):
     
     compiler, runtime = find_cs_binaries()
     
@@ -66,44 +153,35 @@ def run_cs(filename, extension, args):
     # ... execute and delete
     cmd += "({} '{}' {}; rm -f '{}')".format(runtime, tmp, get_sub_args(args), tmp)
     
-    run(cmd, extension, args)
+    run(cmd, recipe, args)
 
-recipes = {}
-
-# 
-#recipes[""] = {
-#    "cordova": "cordova prepare browser"
-#}
-
-recipes["cpp"] = "c++ -Wall -Wextra -std=c++14 {source} -o out && (./out {args}; rm ./out)"
-recipes["c"]   = "gcc -Wall -Wextra -std=c11 {source} -o out && (./out {args}; rm ./out)"
-recipes["js"]  = "node {source} {args}"
-recipes["scpt"]  = "osascript {source} {args}"
-recipes["php"] = "php {source} {args}"
-recipes["cs"]  = run_cs
-recipes["sh"]  = "sh {source} {args}"
-recipes["bash"]  = "bash {source} {args}"
-recipes["zsh"]  = "zsh {source} {args}"
-recipes["mm"]  = "clang++ -std=c++14 -ObjC++ -framework Foundation {source} -o out && (./out {args}; rm ./out)"
-recipes["r"]   = "/Library/Frameworks/R.framework/Resources/Rscript {source} {args}" 
-recipes["npm"] = "npm start --prefix {source}"
-
-recipes["py"]  = "python3 {source} {args}"
-
-recipes["matlab"] = "matlab -nodisplay -nosplash -nodesktop -noFigureWindows -r \"try, run('{source}'), catch e, fprintf('%s\\n', e.message), end;exit(0);\""
-recipes["objc"] = "clang -framework Foundation {source} -o out && (./out {args}; rm ./out)"
-
-
-recipes["m"] = ["matlab", "objc"]
-
-recipes["java"] = run_java
 
 def error(code, str):
     sys.stderr.write(str + "\n")
     sys.exit(code) 
     
-def run(cmd, extension, args):
+def execute_recipe(recipes, filename, args):
     
+    if len(recipes) == 1:
+        recipe = recipes[0]
+        
+        # Fancy recipe, requires helper code to run    
+        if callable(recipe["run"]):
+            recipe["run"](filename, recipe, args)
+        
+        # Execute recipe based on a string
+        else:
+            run(recipe["run"].format(source=filename, args=get_sub_args(args)), recipe, args)
+    else:
+        errormessage = "recipe is ambigious, try one of the following:\n";
+        
+        for r in recipes:
+            errormessage += "  {} {} {} [args...]\n".format(os.path.basename(sys.argv[0]), r["recipe"], filename)
+        
+        error(3, errormessage)
+        
+def run(cmd, recipe, args):
+
     if args.bench:
         ruler = "----------------------"
         
@@ -112,76 +190,71 @@ def run(cmd, extension, args):
         cmd = "TIMEFORMAT=\"\n{0}\ntook %R seconds starting at $(date +'%T')\"; echo '{0}'; time {1}; unset TIMEFORMAT;".format(ruler, cmd)
     
     if args.entr:
-        entr_cmd = "find . -type f -name '*{}' -maxdepth {} |  entr -c -r sh -c '{}';"
+        entr_cmd = "find . -type f {} -maxdepth {} |  entr -c -r sh -c '{}';"
+        pattern = ""
+        
+        for i, ext in enumerate(recipe["ext"]):
+            
+            if i > 0:
+                pattern += "-o "
+            
+            pattern += "-name '*{}' ".format(ext)
         
         escaped_cmd = cmd.replace("'", "'\\''")
                 
-        os.system(entr_cmd.format(extension, args.maxdepth, escaped_cmd))
+        os.system(entr_cmd.format(pattern, args.maxdepth, escaped_cmd))
     
     else:
         os.system(cmd)
     
-def get_recipe_from_path(path):
+# TODO: gerjo: this could be part of the recipe setup. Specify which 
+# files to sense for in order to deduce how to execute a path.  
+def deduce_recipes_from_path(path):
     
     # Node projects have a package file.
     if os.path.isfile(os.path.join(path, "package.json")):
-        return "npm"
+        return [r for r in get_recipes() if r["recipe"] == "npm"]
     
-    return None
+    return []
     
 def main(args):
 
     filename = args.filename
     pwd = os.getcwd()
    
-    extension = os.path.splitext(filename)[1].lower().lstrip(".")
+    extension = os.path.splitext(filename)[1].lower()
    
     # Use explicitly defined recipe when provided
     if args.recipe:
-        recipe = args.recipe
+        recipes = [r for r in get_recipes() if r["recipe"] == args.recipe]
+        
+        if len(recipes) > 0:
+            execute_recipe(recipes, filename, args)
+        else:
+            error(7, "Requested recipe '{}' does not exist.".format(args.recipe))
         
     # otherwise deduce it from file extension
     elif extension is not "":
-        recipe = extension
+        recipes = [r for r in get_recipes() if extension in r["ext"]]
     
+        if len(recipes) > 0:
+            execute_recipe(recipes, filename, args)
+        else:
+            error(8, "No recipe available for file extension '{0}'.".format(extension))
+       
     # Deduce it from path    
     elif os.path.isdir(filename):
-        recipe = get_recipe_from_path(filename)
+        recipes = deduce_recipes_from_path(filename)
     
-        if recipe is None:
-            error(6, "Cannot determine recipe based on path '{}'.".format(filename))
-    
-    # Custom error in case file does not even exist
-    elif not os.path.isfile(filename):
-        error(5, "Cannot determine recipe for executing '{}', file does not exist.".format(filename))
-        
-    # Possibly only reached through programming error
-    else:
-        error(4, "Cannot determine recipe for executing '{}'.".format(filename))
-        
-        
-    if recipe in recipes:
-        
-        # Ambiguous recipe. List the alternatives
-        if isinstance(recipes[recipe], list):
-            errormessage = "recipe '{}' is ambigious, try the following:\n".format(recipe);
-            
-            for key in recipes[recipe]:
-                errormessage += "  {} {} {} [args...]\n".format(os.path.basename(sys.argv[0]), key, filename)
-            
-            error(3, errormessage)
-        
-        # Fancy recipe, requires helper code to run    
-        elif callable(recipes[recipe]):
-            recipes[recipe](filename, extension, args)
-            
-        # Execute recipe based on a string    
+        if len(recipes) > 0:
+            execute_recipe(recipes, filename, args)
         else:
-            run(recipes[recipe].format(source=filename, args=get_sub_args(args)), extension, args)
+            error(6, "Cannot determine recipe based on path '{}'.".format(filename))
             
+    # This code shouldn't normally be reached
     else:
-        error(2, "Cannot execute '{}' files. No known recipe.".format(recipe))
-    
+        error(2, "Cannot execute '{}'. No known recipe could be deduced.".format(filename))   
+        
         
 parser = argparse.ArgumentParser(description="Execute any sort of file.", epilog="This ought to make it easier to quickly test something, right?")
 parser.add_argument("recipe", help="The recipe to use in case file extension is ambiguous", nargs="?", default=None)
