3669e0f144a8b7bd35438723152de219785a9cc8
Gerard <gerjoo+git@gmail.com>
2021-05-21 09:24:58 +0100

    More random C# code

--- wisdom.cs
+++ wisdom.cs
@@ -2,20 +2,26 @@ using System;
 
 
 public class Program {
-	public static void Main(string[] args) {
-		
-		bool result = true;
-		
-		result &= BooleanFunction("a", true);
-		result &= BooleanFunction("b", true);
-		result &= BooleanFunction("c", true);
-		
-		System.Console.WriteLine(string.Format("Final boolean result: {0}", result));
+	
+	public enum MyEnum {
+		Gerard,
+		Purple,
+		Monkey,
+		Dishwasher,
+	};
+	
+	public class MyClass<T> {
+		public void Operation(T value) {
+			System.Console.WriteLine(value);
+		}
 	}
 	
-	private static bool BooleanFunction(string name, bool outcome) {
-		System.Console.WriteLine(string.Format("BooleanFunction({0})", name));
+	public static void Main(string[] args) {
 		
-		return outcome;
+		MyClass<MyEnum> worker = new MyClass<MyEnum>();
+		
+		worker.Operation(MyEnum.Monkey);
+		
+		System.Console.WriteLine("good");
 	}
 }
